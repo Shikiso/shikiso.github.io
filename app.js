7acd8df201262b9516d8276313ccf8f0c73dbfe0
shikiso <lewisemerson00@gmail.com>
2024-10-15 22:25:19 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const publicDirectory = path.dirname(__dirname, "./public");
+const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static('.'));
+app.use(express.urlencoded({ extended: 'false' }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 5000");
+});
